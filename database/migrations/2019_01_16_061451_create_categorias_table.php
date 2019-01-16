f7f5dd48e161ca7da899fef573fa6974d95f097d
Roberto Leon <roberto.leon.armario@gmail.com>
2019-01-16 10:15:27 +0100

    prueba

--- database/migrations/2019_01_16_061451_create_categorias_table.php
+++ database/migrations/2019_01_16_061451_create_categorias_table.php
@@ -15,7 +15,7 @@ class CreateCategoriasTable extends Migration
     {
         Schema::create('categorias', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nombre');
+            $table->string('nombrett');
             $table->timestamps();
         });
     }
