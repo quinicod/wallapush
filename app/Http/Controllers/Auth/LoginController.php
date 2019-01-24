a92650e6143b29c2174e8c374a389aa084e796c5
Roberto Leon <roberto.leon.armario@gmail.com>
2019-01-24 09:14:53 +0100

    validacion con campo actived

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use app\User;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,13 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            $this->username() => 'required|string',
+            'password' => 'required|string',
+            'actived' => '1',
+        ]);
     }
 }
