3847ce1cb4bd246a18f300320be21654a15aa972
Roberto Leon <roberto.leon.armario@gmail.com>
2019-01-24 09:38:02 +0100

    modificacion tabla users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,6 @@ class CreateUsersTable extends Migration
             $table->string('localidad');
             $table->float('saldo')->default(25);
             $table->boolean('actived')->default(false);
-            $table->string('codigo_confirmacion')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
